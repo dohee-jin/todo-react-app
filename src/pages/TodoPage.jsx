94b9652b55dca06d4fd15a3e58a596751b4cdfae
dohee-jin <doheeeq@gmail.com>
2026-06-18 21:13:18 +0900

    feat: toto 입력폼 추가

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,40 +1,28 @@
 import { useState } from "react";
 import { useTodoStore } from "../store/TodoStore.js";
 import TodoItem from "../components/TodoItem.jsx";
+import AddTodo from "../components/AddTodo.jsx";
 
 const TodoPage = () => {
-    const [input, setInput] = useState("");
-    const todos = useTodoStore((state) => state.todos);
-    const addTodo = useTodoStore((state) => state.addTodo);
 
-    const handleAdd = () => {
-        if (!input.trim()) return;
-        addTodo(input.trim());
-        setInput("");
-    };
+    const todos = useTodoStore((state) => state.todos);
 
     return (
         // addtodo 컴포넌트로 변경
-        <div>
-            <h1 className="text-2xl font-bold mb-4">Todo List</h1>
-            <div className="flex gap-2 mb-4">
-                <input
-                    value={input}
-                    onChange={(e) => setInput(e.target.value)}
-                    onKeyDown={(e) => e.key === "Enter" && handleAdd()}
-                    className="flex-1 p-2 border rounded"
-                    placeholder="할 일을 입력하세요"
-                />
-                <button onClick={handleAdd} className="px-4 bg-violet-600 text-white rounded">
-                    추가
-                </button>
+        <>
+            <h1 className="text-2xl font-bold mb-4">My Task</h1>
+            <div>
+                <AddTodo />
+                <ul className="space-y-3">
+                    {todos.length > 0 ? (
+                        todos.map((todo) => (
+                            <TodoItem key={todo.id} todo={todo}/>
+                        ))
+                        ) : <p className='text-center text-gray-400 py-10'>등록된 할 일이 없습니다.</p>
+                    }
+                </ul>
             </div>
-            <ul className="space-y-2">
-                {todos.map((todo) => (
-                    <TodoItem key={todo.id} todo={todo} />
-                ))}
-            </ul>
-        </div>
+        </>
     )
 }
 
